fd233fadfcb834866400bd9bd2eba084e7f5b6f8
Shun Usami <usatie@gmail.com>
2022-04-22 09:03:39 +0900

    :recycle: Refactor main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 18:17:52 by susami            #+#    #+#             */
-/*   Updated: 2022/04/21 23:11:22 by susami           ###   ########.fr       */
+/*   Updated: 2022/04/22 08:57:49 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,16 @@ int	ft_printf(char *format, ...)
 	return (ret);
 }
 
+#define BUF_SIZE 1024
+#define FMT_WIDTH 50
 #define F(...) ({\
 	fflush(stdout);\
-	char s[30];\
-	memset(s, ' ', 30);\
-	strcpy(s, "F(");\
-	strcpy(s + 2, #__VA_ARGS__);\
-	strcpy(s + 2 + strlen(#__VA_ARGS__), ")");\
-	printf("%-30s: stdout=[", s);\
+	char buf[BUF_SIZE];\
+	bzero(buf, BUF_SIZE);\
+	strcpy(buf, "F(");\
+	strcpy(buf + 2, #__VA_ARGS__);\
+	strcpy(buf + 2 + strlen(#__VA_ARGS__), ")");\
+	printf("%-*s: stdout=[", FMT_WIDTH, buf);\
 	fflush(stdout);\
     int ret = _F(__VA_ARGS__);\
 	fflush(stdout);\
@@ -45,18 +47,25 @@ int	ft_printf(char *format, ...)
 })
 
 #ifdef FT_PRINTF
-# define _F(...) ({ft_printf(__VA_ARGS__);})
+# define _F(...) ft_printf(__VA_ARGS__);
 #else
-# define _F(...) ({printf(__VA_ARGS__);})
+# define _F(...) printf(__VA_ARGS__);
 #endif
+
 
 int	main(void)
 {
+    F("hello world");
+	F("%c", 'h');
 	F("%s", "hello world");
-	F("%i", -42);
-	F("%d", -42);
-	F("%u", 42);
 	F("%p", (void *)42);
+	F("%d", -42);
+	F("%i", -42);
+	F("%u", 42);
+	F("%x", 42);
+	F("%X", 42);
+    F("%c %s", 'h', "hello world");
+    F("%c %s %p %d %i %u %x %X", 'h', "hello world", (void *)42, -42, -42, 42, 42, 42);
 
 	return (0);
 }
